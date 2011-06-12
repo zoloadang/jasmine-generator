1964e1d0b8aeffc88a8c1a1b8d7075b795a2af3e
nanzhi <nanzhienai@163.com>
2011-06-12 15:08:51 +0800

    concat path to create

--- create.py
+++ create.py
@@ -1,9 +1,42 @@
 #coding:utf-8
 #author nanzhi<nanzhienai@163.com>
 
-import re, os, sys, shutil
+import re, os, sys, itertools
 import os.path
-from path.relative import relpath
+
+#get relative path (learn from http://hi.baidu.com/tinylee/blog/item/763177c6820444199c163da8.html, thanks)
+def all_equal(elements):
+    return len(set(elements)) == 1
+
+def common_prefix(*sequences):
+    # if there are no sequences at all, we're done
+	#print sequences
+	if not sequences: 
+		return [], []
+    # loop in parallel on the sequences 
+	common = []
+	for elements in itertools.izip(*sequences): 
+		#print elements
+        # unless all elements are equal, bail out of the loop 
+		if not all_equal(elements): 
+			break 
+        # got one more common element, append it and keep looping 
+        common.append(elements[0])
+    # return the common prefix and unique tails 
+	#print common
+	#print [ sequence[len(common) - 1:] for sequence in sequences ]
+	return common, [ sequence[len(common) - 1:] for sequence in sequences ]
+
+def relpath(p1, p2, sep=os.path.sep, pardir=os.path.pardir):
+	common, (u1, u2) = common_prefix(p1.split(sep), p2.split(sep))
+	if not common:
+		return p2 
+	if u2[0] == '.':
+		u2 = u2[1:]
+		length = len(u1) - 1
+	else:
+		length = len(u1)
+	return sep.join([pardir] * length + u2)
 
 #get arg
 def getArg():
